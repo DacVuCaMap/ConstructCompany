a7d1de4aed29fcf23c7e66d864b5ed58dc4f311d
hoangduchai201 <hoangduchai201@gmail.com>
2024-04-09 00:26:44 +0700

    order details

--- server/ConStructCompany/src/main/java/com/app/ConStructCompany/Repository/OrderDetailRepository.java
+++ server/ConStructCompany/src/main/java/com/app/ConStructCompany/Repository/OrderDetailRepository.java
@@ -4,6 +4,9 @@ import com.app.ConStructCompany.Entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+    List<OrderDetail> findAllById(Long id);
 }
