7e024903a0752edc253d60cd0404bc189d5aa49b
hoangduchai201 <hoangduchai201@gmail.com>
2024-04-08 23:40:38 +0700

    sdsds

--- server/ConStructCompany/src/main/java/com/app/ConStructCompany/Request/AddOrderRequest.java
+++ server/ConStructCompany/src/main/java/com/app/ConStructCompany/Request/AddOrderRequest.java
@@ -3,6 +3,7 @@ package com.app.ConStructCompany.Request;
 import com.app.ConStructCompany.Request.dto.OrderDetailDto;
 import com.app.ConStructCompany.Request.dto.OrderDto;
 import lombok.Data;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -11,24 +12,25 @@ public class AddOrderRequest {
     private OrderDto order;
     private List<OrderDetailDto> orderDetails;
 
-    public Boolean isValidRequest(){
-        if (!(
-                Double.isNaN(order.getTotalAmount()) || order.getTotalAmount() <= 0 ||
-                        Double.isNaN(order.getTotalCost()) || order.getTotalCost() <= 0 ||
-                        Double.isNaN(order.getTax()) || order.getTax() <= 0 ||
-                        order.getPositionCustomer().isEmpty() ||
-                        order.getPositionSeller().isEmpty() ||
-                        order.getRepresentativeCustomer().isEmpty() ||
-                        order.getRepresentativeSeller().isEmpty() ||
+    public Boolean isValidRequest() {
+        if (order != null && orderDetails != null &&
+                !(Double.isNaN(order.getTotalAmount()) || order.getTotalAmount() < 0 ||
+                        Double.isNaN(order.getTotalCost()) || order.getTotalCost() < 0 ||
+                        Double.isNaN(order.getTax()) || order.getTax() < 0 ||
+                        order.getPositionCustomer() == null || order.getPositionCustomer().isEmpty() ||
+                        order.getPositionSeller() == null || order.getPositionSeller().isEmpty() ||
+                        order.getRepresentativeCustomer() == null || order.getRepresentativeCustomer().isEmpty() ||
+                        order.getRepresentativeSeller() == null || order.getRepresentativeSeller().isEmpty() ||
                         order.getCustomerId() == null ||
                         order.getSellerId() == null ||
                         orderDetails.isEmpty() ||
                         orderDetails.stream().anyMatch(detail -> detail.getProductId() == null ||
-                                Double.isNaN(detail.getMaterialWeight()) || detail.getMaterialWeight() <= 0 ||
-                                Double.isNaN(detail.getPrice()) || detail.getPrice() <= 0)
-        )) {
+                                Double.isNaN(detail.getMaterialWeight()) || detail.getMaterialWeight() < 0 ||
+                                Double.isNaN(detail.getPrice()) || detail.getPrice() < 0)
+                )) {
             return true;
         }
         return false;
     }
+
 }
