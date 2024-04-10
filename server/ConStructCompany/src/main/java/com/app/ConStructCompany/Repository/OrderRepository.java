98c6ffc1506693555ae720f5a77e52abe81397da
hoangduchai201 <hoangduchai201@gmail.com>
2024-04-10 23:28:04 +0700

    orders

--- server/ConStructCompany/src/main/java/com/app/ConStructCompany/Repository/OrderRepository.java
+++ server/ConStructCompany/src/main/java/com/app/ConStructCompany/Repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package com.app.ConStructCompany.Repository;
 
 import com.app.ConStructCompany.Entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Order findFirstByOrderByOrderCodeDesc();
+    Page<Order> findAllByOrderCodeLikeIgnoreCase(String orderCode, Pageable pageable);
+    Page<Order> findAll(Pageable pageable);
+
 }
