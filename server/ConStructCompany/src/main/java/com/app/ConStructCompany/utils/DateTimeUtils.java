0f0c8e1a1cb735e1b97e7f8e477b7d1e354cbc2e
hoangduchai201 <hoangduchai201@gmail.com>
2024-04-14 21:31:52 +0700

    update

--- server/ConStructCompany/src/main/java/com/app/ConStructCompany/utils/DateTimeUtils.java
+++ server/ConStructCompany/src/main/java/com/app/ConStructCompany/utils/DateTimeUtils.java
@@ -1,5 +1,6 @@
 package com.app.ConStructCompany.utils;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateTimeUtils {
@@ -7,5 +8,9 @@ public class DateTimeUtils {
 
     public static Date getCurrentDate(){
         return new Date();
+    }
+    public static String getDateFormat(String pattern) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        return dateFormat.format(getCurrentDate());
     }
 }
